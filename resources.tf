5b2e443ed80b4a6383a662e146d3254f1fefdc6b
phirmware <chibuzor.ojukwu@gmail.com>
2020-02-07 04:23:51 +0100

    added subnets to the vpc

--- resources.tf
+++ resources.tf
@@ -6,3 +6,9 @@ resource "aws_vpc" "environment-example-two" {
       Name = "terraform-aws-vpc-example-two"
   }
 }
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-west-2a"
+}
