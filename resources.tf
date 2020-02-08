3c26616fb3b8bd327205107e3de878228eddf3e1
phirmware <chibuzor.ojukwu@gmail.com>
2020-02-08 06:52:07 +0100

    added tfvarsfile to gitignore

--- resources.tf
+++ resources.tf
@@ -1,14 +1,14 @@
 resource "aws_vpc" "environment-example-two" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
   tags = {
-      Name = "terraform-aws-vpc-example-two"
+    Name = "terraform-aws-vpc-example-two"
   }
 }
 
 resource "aws_subnet" "subnet1" {
-  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
-  vpc_id = "${aws_vpc.environment-example-two.id}"
+  cidr_block        = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id            = "${aws_vpc.environment-example-two.id}"
   availability_zone = "us-west-2a"
 }
