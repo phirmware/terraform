63cf8830064cd8ce08cc8b975404518efb3731e1
phirmware <chibuzor.ojukwu@gmail.com>
2020-02-08 06:44:37 +0100

    added vars file for terraform variables

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "newvariable" {
+  type    = "string"
+  default = "defaultvalue"
+}
+
+variable "maptype" {
+  type = "map"
+  default = {
+
+  }
+}
+
+variable "instancename" {
+  type    = "string"
+  default = "instancedefaultname"
+}
+
+
